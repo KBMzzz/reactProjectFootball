faceef2290a8f33e398b3c4093fc6971c1a6f4e6
Chris Vaz <chrisvaz17@gmail.com>
2023-09-05 17:41:49 -0400

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "../App.css";
 
 const Footer = () => {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <h4>Created by Chris Vaz @ Juno College</h4>
+    </div>
+  );
 };
 
 export default Footer;
